37f8052cd5727f2e6e94584000ea767166e29bec
Ashton <ashton@platform45.com>
2019-02-26 15:38:39 +0200

    platform specifig camera config

--- recorder.rb
+++ recorder.rb
@@ -9,23 +9,24 @@ class Recorder
 
   def record
     timestamp = Time.now.to_i
-    ffmpeg_cmd = [
-      "ffmpeg",
-      "-framerate 30",
-      "-f avfoundation",
-      '-i "0"',
-      "-map 0",
-      "-b:v 3000k",
-      "-minrate 3000k",
-      "-maxrate 3000k",
-      "-quality realtime",
-      "-crf 18",
-      "-f segment -segment_time 10",
-      "-segment_format mp4",
-      "-strftime 1",
-      "-loglevel quiet",
-      '"./recordings/rubyvid-%s.mp4"'
-    ];
+    ffmpeg_cmd = []
+    ffmpeg_cmd << "ffmpeg"
+    ffmpeg_cmd << "-framerate 30"
+    ffmpeg_cmd << "-f avfoundation" if RUBY_PLATFORM.include?('darwin')
+    ffmpeg_cmd << "-f video4linux2" if RUBY_PLATFORM.include?('linux')
+    ffmpeg_cmd << '-i "0"' if RUBY_PLATFORM.include?('darwin')
+    ffmpeg_cmd << '-i /dev/video0' if RUBY_PLATFORM.include?('linux')
+    ffmpeg_cmd << "-map 0"
+    ffmpeg_cmd << "-b:v 3000k"
+    ffmpeg_cmd << "-minrate 3000k"
+    ffmpeg_cmd << "-maxrate 3000k"
+    ffmpeg_cmd << "-quality realtime"
+    ffmpeg_cmd << "-crf 18"
+    ffmpeg_cmd << "-f segment -segment_time 10"
+    ffmpeg_cmd << "-segment_format mp4"
+    ffmpeg_cmd << "-strftime 1"
+    ffmpeg_cmd << "-loglevel quiet"
+    ffmpeg_cmd << '"./recordings/rubyvid-%s.mp4"'
     IO.popen(ffmpeg_cmd.join ' ')
     auto_clean_up
     true
